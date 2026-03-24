b87731668883798e99ac2fcd6ddc686cfe499285
David de León Acosta <dadeleac@gmail.com>
2026-03-24 15:33:40 +0100

    feat: add validation pipeline and custom validation exception

---
+++ ProjectManagement.Application/Common/Errors/ApplicationErrors.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectManagement.Application.Common.Errors
+{
+    public static class ApplicationErrors
+    {
+        public const string ValidationFailed = "validation.failed";
+        public const string ValidationSummary = "validation.one_or_more_errors_occurred";
+    }
+}
---
+++ ProjectManagement.Application/Common/Exceptions/RequestValidationException.cs
@@ -0,0 +1,25 @@
+﻿using ProjectManagement.Application.Common.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Application.Common.Exceptions
+{
+    public sealed class RequestValidationException : Exception
+    {
+        public string Code { get; }
+        public IReadOnlyCollection<ValidationError> Errors { get; }
+
+        public RequestValidationException(string code, IEnumerable<ValidationError> errors)
+            : base(ApplicationErrors.ValidationSummary)
+        {
+            Code = code;
+            Errors = (errors ?? Enumerable.Empty<ValidationError>())
+                .ToList()
+                .AsReadOnly();
+        }
+
+    }
+}
