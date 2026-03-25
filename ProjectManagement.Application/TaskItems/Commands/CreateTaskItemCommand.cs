a12ef9ad4e68009f10ad7b5759a0e315c3f57c2b
David de León Acosta <dadeleac@gmail.com>
2026-03-25 13:55:33 +0100

    feat(application): implement task item creation logic

---
+++ ProjectManagement.Application/TaskItems/Commands/CreateTaskItemCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using ProjectManagement.Domain.Enums;
+
+namespace ProjectManagement.Application.Tasks.Commands
+{
+    public sealed record CreateTaskItemCommand(
+        Guid ProjectId,
+        string Title,
+        string? Description,
+        TaskPriority Priority,
+        DateTime? DueDate) : IRequest<Guid>;
+}
