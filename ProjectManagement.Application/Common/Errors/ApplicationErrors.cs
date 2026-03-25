193e7e29efa567d6df069604a0a4371c280859bc
David de León Acosta <dadeleac@gmail.com>
2026-03-25 13:54:44 +0100

    feat(application): implement validation pipeline and core abstractions

--- ProjectManagement.Application/Common/Errors/ApplicationErrors.cs
+++ ProjectManagement.Application/Common/Errors/ApplicationErrors.cs
@@ -4,5 +4,7 @@
     {
         public const string ValidationFailed = "validation.failed";
         public const string ValidationSummary = "validation.one_or_more_errors_occurred";
+        public const string NotFoundCode = "entity.not_found";
+        public const string NotFoundSummary = "entity.not_found_message";
     }
 }
